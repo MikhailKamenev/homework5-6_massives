20f95481f29fba9c46091890d42b1162b0b74419
Mikhail <kamenevmm@gmail.com>
2022-08-07 00:36:20 +0300

    homework is done

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
-        // сегодня поработаем с массивами
-        // объявление массива. надо задать тип массива (только переменные этого типа могут храниться в массиве)
-
-        int[] blackHole = new int[100];
-        char[] chars = new char[15];
-        double[] doubleArr = new double[10];
-        long[] numbers = new long[82];
-        boolean[] bools = new boolean[2];
-
-        // нумерация ячеек в массиве идет от 0. т.е. в массиве blackHole первое значение будет 0, а последнее 100
-        // чтобы узнать длину массива надо обратиться к его свойству .lenght
-
-        int blackHoleLenght = blackHole.length;
-        System.out.println(blackHoleLenght);
-
-        // чтобы узнать значение переменной в массиве, надо воспользоваться ее индексом в массиве
-
-        int i = blackHole[99];
-        System.out.println(i);
-
-        // чтобы задать значение переменной в массиве: в скобках индекс, после оператора присваивания значение
-
-        blackHole[67] = 23;
 
         // task1
 
@@ -55,7 +30,36 @@ public class Main {
             }
         }
         //task3
+        for (int a = superMassive.length-1; a >= 0; a = a-1) {
+            if (a == 0){
+                System.out.println(superMassive[a]);}
+            else
+            {System.out.print(superMassive[a] + ", ");}}
 
+        for (int a = doubleMassive.length-1; a >= 0; a = a-1){
+            if (a==0){
+                System.out.println(doubleMassive[a]);}
+            else {
+                System.out.print(doubleMassive[a] + ", ");
+            }}
+
+        for (int a = toBeOrNotToBe.length -1; a >= 0; a--){
+            if (a==0){
+                System.out.println(toBeOrNotToBe[a]);}
+            else {
+                System.out.print(toBeOrNotToBe[a] + ", ");}
+        }
+
+        //task4
+        //int[] superMassive = new int[]{1,2,3};
+        for(int a = 0; a < superMassive.length; a++){
+            if (superMassive[a]%2 != 0){
+                superMassive[a] = superMassive[a] + 1;
+                System.out.print(superMassive[a]);}
+            else {
+                System.out.print(", " + superMassive[a] + ", ");
+            }
+        }
 
     }
 }
